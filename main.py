5b1eb629676ded29fa1267dca984042941824ff7
Bryce VE <brycevanewyk4@gmail.com>
2023-07-17 14:53:01 +1000

    added control handler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from player import Player
+from control_handler import player_input
 
 # Setup
 pygame.init()
@@ -15,30 +16,6 @@ units = 20
 player = Player(mid_width, mid_height, screen)
 
 
-def controls():
-    if player.can_move():
-        player.moving = True
-
-        up = [pygame.K_w, pygame.K_UP]
-        down = [pygame.K_s, pygame.K_DOWN]
-        left = [pygame.K_a, pygame.K_LEFT]
-        right = [pygame.K_d, pygame.K_RIGHT]
-
-        keys = pygame.key.get_pressed()
-
-        if keys[up[0]] or keys[up[1]]:
-            player.move(0, -units)
-        if keys[down[0]] or keys[down[1]]:
-            player.move(0, units)
-
-        if keys[left[0]] or keys[left[1]]:
-            player.move(-units, 0)
-        if keys[right[0]] or keys[right[1]]:
-            player.move(units, 0)
-
-        player.moving = False
-
-
 # Main loop, loops every frame
 while running:
     # Events, loops through all events in event queue
@@ -50,7 +27,7 @@ while running:
     screen.fill("black")
 
     # Functionality here
-    controls()
+    player_input(player, units)
 
     # Render last
     player.draw_player()

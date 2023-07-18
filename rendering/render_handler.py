91f7ce20a35ff73139c75bcd28c77b7001d6df06
BAVDE1 <bavde4@gmail.com>
2023-07-18 18:40:12 +1000

    fixed level alignment

--- rendering/render_handler.py
+++ rendering/render_handler.py
@@ -14,16 +14,17 @@ def parse_level(level_source):
     return level_lines
 
 
-def render_level(level_to_render, unit, screen_width, screen_height, surface):
+def render_level(level_to_render, unit, surface):
     level_lines = parse_level(level_to_render)
 
     line_num = 0
     for line in level_lines:
-        line_num += 1
         char_num = 0
+        line = line[1:-2]  # strips square brackets
         for char in line:
-            char_num += 1
-            pos_x = (screen_width / 2 * unit) + (unit * char_num)
-            pos_y = (screen_height / 2) + (unit * line_num)
+            pos_x = (unit * -.5) + (unit * char_num)
+            pos_y = (unit * -.5) + (unit * line_num)
             if char == ".":
                 pygame.draw.rect(surface, "green", (pos_x, pos_y, unit, unit))
+            char_num += 1
+        line_num += 1

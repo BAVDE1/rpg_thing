d0479cb47f7048c4a598503bb3223f36e769338f
Bryce VE <brycevanewyk4@gmail.com>
2023-08-03 13:41:20 +1000

    fixed tweener

--- utility/tweening.py
+++ utility/tweening.py
@@ -7,19 +7,19 @@ class LinearTween:
         self.start_time = self.current_time
         self.end_time = self.start_time + duration
 
+        self.value = value
         self.start_value = value
         self.end_value = goal - value
 
     def get_percent(self):
         return (self.current_time - self.start_time) / (self.end_time - self.start_time)
 
-    def update(self, item):
+    def update(self):
         self.current_time = time.time()
         if not self.has_reached_goal():
-            item = self.start_value + (self.end_value * self.get_percent())
+            self.value = self.start_value + (self.end_value * self.get_percent())
         else:
-            item = self.end_value
-        return item
+            self.value = self.end_value
 
     def has_reached_goal(self):
         return self.end_time < self.current_time

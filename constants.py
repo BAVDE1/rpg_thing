3944b5fdfd935c2acff8a1175e551f965d8d570c
BAVDE1 <bavde4@gmail.com>
2023-08-12 20:12:26 +1000

    broke some stuff

--- constants.py
+++ constants.py
@@ -62,27 +62,29 @@ GRASS_SPRITE = pg.transform.scale(pg.image.load(GRASS_TEXTURE), (UNIT, UNIT))
 
 EDGE_STRAIGHT = "assets/textures/tiles/edge_straight.png"
 EDGE_STRAIGHT_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT), (UNIT, UNIT))
-EDGE_STRAIGHT_CORNER = "assets/textures/tiles/edge_straight_corner.png"
-EDGE_STRAIGHT_CORNER_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT_CORNER), (UNIT, UNIT))
-EDGE_STRAIGHT_CORNERS = "assets/textures/tiles/edge_straight_corners.png"
-EDGE_STRAIGHT_CORNERS_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT_CORNERS), (UNIT, UNIT))
-EDGE_STRAIGHT_STRAIGHT = "assets/textures/tiles/edge_straight_straight.png"
-EDGE_STRAIGHT_STRAIGHT_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT_STRAIGHT), (UNIT, UNIT))
+EDGE_SINGLE = "assets/textures/tiles/edge_single.png"
+EDGE_SINGLE_SPRITE = pg.transform.scale(pg.image.load(EDGE_SINGLE), (UNIT, UNIT))
+# EDGE_STRAIGHT_CORNER = "assets/textures/tiles/edge_straight_corner.png"
+# EDGE_STRAIGHT_CORNER_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT_CORNER), (UNIT, UNIT))
+# EDGE_STRAIGHT_CORNERS = "assets/textures/tiles/edge_straight_corners.png"
+# EDGE_STRAIGHT_CORNERS_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT_CORNERS), (UNIT, UNIT))
+# EDGE_STRAIGHT_STRAIGHT = "assets/textures/tiles/edge_straight_straight.png"
+# EDGE_STRAIGHT_STRAIGHT_SPRITE = pg.transform.scale(pg.image.load(EDGE_STRAIGHT_STRAIGHT), (UNIT, UNIT))
 
 EDGE_CORNER = "assets/textures/tiles/edge_corner.png"
 EDGE_CORNER_SPRITE = pg.transform.scale(pg.image.load(EDGE_CORNER), (UNIT, UNIT))
-EDGE_CORNERS_OPP = "assets/textures/tiles/edge_corners_opp.png"
-EDGE_CORNERS_OPP_SPRITE = pg.transform.scale(pg.image.load(EDGE_CORNERS_OPP), (UNIT, UNIT))
-EDGE_CORNERS_INL = "assets/textures/tiles/edge_corners_inl.png"
-EDGE_CORNERS_INL_SPRITE = pg.transform.scale(pg.image.load(EDGE_CORNERS_INL), (UNIT, UNIT))
+# EDGE_CORNERS_OPP = "assets/textures/tiles/edge_corners_opp.png"
+# EDGE_CORNERS_OPP_SPRITE = pg.transform.scale(pg.image.load(EDGE_CORNERS_OPP), (UNIT, UNIT))
+# EDGE_CORNERS_INL = "assets/textures/tiles/edge_corners_inl.png"
+# EDGE_CORNERS_INL_SPRITE = pg.transform.scale(pg.image.load(EDGE_CORNERS_INL), (UNIT, UNIT))
 
 EDGE_ICORNER = "assets/textures/tiles/edge_icorner.png"
 EDGE_ICORNER_SPRITE = pg.transform.scale(pg.image.load(EDGE_ICORNER), (UNIT, UNIT))
-EDGE_ICORNER_CORNER = "assets/textures/tiles/edge_icorner_corner.png"
-EDGE_ICORNER_CORNER_SPRITE = pg.transform.scale(pg.image.load(EDGE_ICORNER_CORNER), (UNIT, UNIT))
-
-EDGE_END = "assets/textures/tiles/edge_end.png"
-EDGE_END_SPRITE = pg.transform.scale(pg.image.load(EDGE_END), (UNIT, UNIT))
+# EDGE_ICORNER_CORNER = "assets/textures/tiles/edge_icorner_corner.png"
+# EDGE_ICORNER_CORNER_SPRITE = pg.transform.scale(pg.image.load(EDGE_ICORNER_CORNER), (UNIT, UNIT))
+#
+# EDGE_END = "assets/textures/tiles/edge_end.png"
+# EDGE_END_SPRITE = pg.transform.scale(pg.image.load(EDGE_END), (UNIT, UNIT))
 
 # ASCII
 ASCII_TO_SPRITE = {
@@ -94,12 +96,8 @@ OUTLINES_STRAIGHT = {
     EDGE_STRAIGHT_SPRITE: [True, False, True, True],
     pg.transform.flip(EDGE_STRAIGHT_SPRITE, 0, 1): [False, True, True, True],
     pg.transform.rotate(EDGE_STRAIGHT_SPRITE, -90): [True, True, True, False],
-    pg.transform.rotate(EDGE_STRAIGHT_SPRITE, 90): [True, True, False, True]
-}
-
-OUTLINES_STRAIGHT_CORNER = {
-    EDGE_STRAIGHT_CORNER: [True, False, True, True,
-                           True, False, True, True]
+    pg.transform.rotate(EDGE_STRAIGHT_SPRITE, 90): [True, True, False, True],
+    EDGE_SINGLE_SPRITE: [False, False, False, False]
 }
 
 OUTLINES_CORNER = {
--- rendering/level.py
+++ rendering/level.py
@@ -7,7 +7,6 @@ NORTH = "n"
 SOUTH = "s"
 EAST = "e"
 WEST = "w"
-
 NORTH_EAST = "ne"
 NORTH_WEST = "nw"
 SOUTH_EAST = "se"
@@ -30,38 +29,34 @@ def parse_level(level_source):
 def outline_decider(dic: dict):
     add = None
     if dic[TILE] is not None:
-        all_args = [bool(dic[NORTH]), bool(dic[SOUTH]), bool(dic[EAST]), bool(dic[WEST]), bool(dic[NORTH_EAST]),
-                    bool(dic[NORTH_WEST]), bool(dic[SOUTH_EAST]), bool(dic[SOUTH_WEST])]
+        all_args = [bool(dic[NORTH]), bool(dic[SOUTH]), bool(dic[EAST]), bool(dic[WEST]),
+                    bool(dic[NORTH_EAST]), bool(dic[NORTH_WEST]), bool(dic[SOUTH_EAST]), bool(dic[SOUTH_WEST])]
         straight_args = all_args[:4]
         corner_args = all_args[4:]
-        outline_data = outline_chooser(all_args, straight_args, corner_args)
+        outline_data = outline_chooser(straight_args, corner_args)
+
         if outline_data and outline_data[0] in outline_data[1].values():
             for key in outline_data[1].keys():
-                print(outline_data)
-
                 if outline_data[1][key] == outline_data[0]:
                     add = key
     return add
 
 
-def outline_chooser(all_args, straight_args, corner_args):
+def outline_chooser(straight_args, corner_args):
     """ Returns [0]: arguments, [1]: constant dict to use """
     if straight_args in OUTLINES_STRAIGHT.values():
         return straight_args, OUTLINES_STRAIGHT
-    if all_args in OUTLINES_STRAIGHT_CORNER.values():
-        return all_args, OUTLINES_STRAIGHT_CORNER
     if straight_args in OUTLINES_ICORNER.values():
         return straight_args, OUTLINES_ICORNER
     if corner_args in OUTLINES_CORNER.values():
         return corner_args, OUTLINES_CORNER
 
 
-def store_layer(layer, layer_lines):
+def store_layer(layer: list, layer_lines):
     for line in layer_lines:
         row = []
-        line = line.strip("]")
         for char in line.strip():
-            row.append(ASCII_TO_SPRITE[char] if char in ASCII_TO_SPRITE else None)
+            row.append(ASCII_TO_SPRITE[char] if char in ASCII_TO_SPRITE else "None!!!!")
         layer.append(row)
 
 
@@ -79,7 +74,6 @@ class Level:
         if not self.is_initialised:
             self.is_initialised = False
             store_layer(self.ground_layer, self.ground_layer_lines)
-
             self.initialise_outline()
             self.is_initialised = True
 
@@ -94,17 +88,27 @@ class Level:
                 e = min(len(column) - 1, col_num + 1)
                 w = max(0, col_num - 1)
 
-                row.append(outline_decider({
-                    TILE: sprite,
-                    NORTH: self.ground_layer[n][col_num],
-                    SOUTH: self.ground_layer[s][col_num],
-                    EAST: self.ground_layer[row_num][e],
-                    WEST: self.ground_layer[row_num][w],
-                    NORTH_EAST: self.ground_layer[n][e],
-                    NORTH_WEST: self.ground_layer[n][w],
-                    SOUTH_EAST: self.ground_layer[s][e],
-                    SOUTH_WEST: self.ground_layer[s][w]
-                }))
+                try:
+                    di = {
+                        TILE: sprite,
+                        NORTH: self.ground_layer[n][col_num],
+                        SOUTH: self.ground_layer[s][col_num],
+                        EAST: self.ground_layer[row_num][e],
+                        WEST: self.ground_layer[row_num][w],
+                        NORTH_EAST: self.ground_layer[n][e],
+                        NORTH_WEST: self.ground_layer[n][w],
+                        SOUTH_EAST: self.ground_layer[s][e],
+                        SOUTH_WEST: self.ground_layer[s][w]}
+                except IndexError:
+                    st = ""
+                    num = 0
+                    for l in self.ground_layer[n]:
+                        st += str(num) + ": " + str(l)
+                        num += 1
+                    raise IndexError(n, s, e, w, st, self.ground_layer)
+
+                row.append(outline_decider(di))
+
                 col_num += 1
             self.outline_layer.append(row)
             row_num += 1
